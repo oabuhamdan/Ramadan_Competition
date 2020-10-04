7567a167b3698b84a86019fb48e405a783f89f87
ohamdan <osamaabuhamdan@yahoo.com>
2020-10-05 01:05:46 +0300

    Fixed some issues

--- users/views.py
+++ users/views.py
@@ -23,16 +23,10 @@ def save_to_db(request):
         pt_type = PointsType.objects.filter(form_type=key).first()
         if pt_type is not None and value == 'on':
             points, pt_details = get_points_and_details(items, key)
-            pt_exists = Point.objects.filter(user=user, type=pt_type, record_date=record_date).first()
             total = total + points
-            if pt_exists is not None:
-                print(pt_exists)
-                pt_exists.value = points
-                pt_exists.details = pt_details
-                pt_exists.save()
-            else:
-                Point.objects.create(user=user, type=pt_type, value=points, details=pt_details,
-                                     record_date=record_date)
+            Point.objects.create(user=user, type=pt_type, value=points, details=pt_details,
+                                 record_date=record_date)
+
     user.total_points = user.total_points + total
     user.save()
 
