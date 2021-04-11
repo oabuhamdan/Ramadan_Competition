392aff1b954a0ae54c39c48182d1c208bf1e04e9
osama-abuhamdan <osamaabuhamdan@yahoo.com>
2021-04-11 14:44:24 +0300

    Support line graph for points

--- users/models.py
+++ users/models.py
@@ -67,8 +67,8 @@ class PointsType(models.Model):
 
 
 class Point(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, related_name='point', null=True)
-    type = models.ForeignKey(PointsType, on_delete=models.SET_NULL, related_name='type', null=True)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='point', null=True)
+    type = models.ForeignKey(PointsType, on_delete=models.CASCADE, related_name='type', null=True)
     value = models.FloatField()
     details = models.CharField(max_length=256, default='')
     record_date = models.IntegerField(default=1)
