cf1513c151bdd6a97745cc846e10249b70bc5240
osama-abuhamdan <osamaabuhamdan@yahoo.com>
2021-05-18 18:18:07 +0300

    Update DB credentials

--- HomeCompetetion/settings.py
+++ HomeCompetetion/settings.py
@@ -13,6 +13,9 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
+import psycopg2
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -75,9 +78,10 @@ WSGI_APPLICATION = 'HomeCompetetion.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {}
-
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
